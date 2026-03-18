f71c40a8c42ec18863781c24436f92b808dde2cb
Almog Sade <almog2019@gmail.com>
2026-03-18 13:30:45 +0200

    testing...

--- main.py
+++ main.py
@@ -14,13 +14,13 @@ import time
 import matplotlib.pyplot as plt
 
 # 1. Setup components
-compressor = HCS3D(10000, sr=[0.5, 0.5, 0.5], Phi_names=["SUBSAMPLING", "SUBSAMPLING", "SUBSAMPLING"], Psi_names=["DCT", "DCT", "DCT"])
+compressor = HCS3D(28800, sr=[0.57, 0.57, 1.0], Phi_names=["SUBSAMPLING", "SUBSAMPLING", "IDENTITY"], Psi_names=["DCT", "DCT", "DCT"])
 logger = DataLogger(compressor_name=compressor.name, compressor_id=compressor.compressor_id, base_dir="results")
 handler = RunHandler(compressor, logger)
 
 # 2. Load HSI
 hsi_data = util.load_hsi("raw\\Indian_pines_corrected.mat")
-# hsi_data = hsi_data[:50, :50, :50]
+hsi_data = hsi_data[:, :, :32]
 
 # 3. Run:
 # a. Single Run:
@@ -33,7 +33,7 @@ handler.run_experiment(hsi_data, dataset_name="IndianPines", save_reconstruction
 #     handler.run_experiment(hsi_data, dataset_name=f"IndianPines")
 
 orig = visuals.to_false_color(hsi_data)
-rec = visuals.to_false_color(visuals.load_reconstruction("results\\HCS 3D\\reconstructions\\220013_IndianPines_recon.npz"))
+rec = visuals.to_false_color(visuals.load_reconstruction("results\\HCS 3D\\reconstructions\\220028_IndianPines_recon.npz"))
 plt.figure()
 plt.subplot(1, 2, 1)
 plt.imshow(orig)
--- sandbox.py
+++ sandbox.py
@@ -4,27 +4,50 @@ import scipy as sp
 import matplotlib.pyplot as plt
 
 
-Phi = measurement_matrices.get_measurement_matrix("SUBSAMPLING", 50, 100)
-Psi = transforms.get_transform("DCT", 100)
-D = Phi @ Psi
-D /= np.linalg.norm(D, axis=0, keepdims=True)
+Z = util.load_hsi("raw\\Indian_pines_corrected.mat")
+Z = Z[:100, :100, :100]
+minval, maxval, depth = util.get_hsi_statistics(Z)
+Z = util.normalize_zero_mean(Z, minval, maxval)
+
+Phi = measurement_matrices.get_measurement_matrix("SUBSAMPLING", 60, 100)
+Psi = transforms.get_inverse_transform("DCT", 100)
 N = 3
 
-S = 40
-Y, X_true = util.generate_block_sparse_signal(D, S, N)
+D = Phi @ Psi
+col_norms = np.linalg.norm(D, axis=0)
+col_norms[col_norms == 0] = 1.0
+S_inv = np.diag(1.0 / col_norms)
+D_norm = D @ S_inv
+
+Y = Z.copy()
+for n in range(N):
+    Y = util.mode_n_product(Y, Phi, n)
 
 Ds = []
 for n in range(N):
-    Dn = D / np.linalg.norm(D, axis=0, keepdims=True)
-    Ds.append(Dn)
+    Ds.append(D_norm)
 
-X_hat = recovery_algorithms.n_bomp(Ds, Y, 2 * S**N)
-Y_hat = X_hat.copy()
+X_hat = recovery_algorithms.n_bomp(Ds, Y, 25000)
+
+Z_hat = X_hat.copy()
+Psi_norm = Psi @ S_inv
 for n in range(N):
-    Y_hat = util.mode_n_product(Y_hat, D, n)
+    Z_hat = util.mode_n_product(Z_hat, Psi_norm , n)
 
+Z = util.denormalize_zero_mean(Z, minval, maxval)
+Z_hat = util.denormalize_zero_mean(Z_hat, minval, maxval)
 
-print(f"RMSE: {util.calc_rmse(Y_hat, Y)}")
-print(f"SAM: {util.calc_sam(Y_hat, Y)}")
+print(f"RMSE: {util.calc_rmse(Z_hat, Z)}")
+print(f"SAM: {util.calc_sam(Z_hat, Z)}")
+
+orig = visuals.to_false_color(Z)
+rec = visuals.to_false_color(Z_hat)
+
+plt.figure()
+plt.subplot(1,2,1)
+plt.imshow(orig)
+plt.subplot(1,2,2)
+plt.imshow(rec)
+plt.show()
 
 

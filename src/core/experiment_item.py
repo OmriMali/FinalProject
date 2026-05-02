4f873d3e41895f9f2badabfd57cd005719f3b375
Almog Sade <almog2019@gmail.com>
2026-05-02 17:07:04 +0300

    Update ExperimentItem

--- src/core/experiment_item.py
+++ src/core/experiment_item.py
@@ -12,52 +12,47 @@ VALID_STATUSES = {
 
 @dataclass
 class ExperimentItem:
-
-
-
-    # Input
+    
+    # Provided on initialization
     hsi: HSI
 
-    # Compressor
     compressor_name: str
     compressor_params: Dict[str, Any]
 
-    # Experiment specifics
+    experiment_machine: str
     save_hsi: bool = False
-    ber: Optional[float] = None
-    experiment_id: Optional[str] = None
+    ber: float = 0
+    tag: Optional[str] = None
 
-    # HSI metadata
-    sensor: Optional[str] = None
-    site: Optional[str] = None
-    name: Optional[str] = None
-
-    # Compression Output
+    # Updated during experiment
     bitstream: Optional[bytes] = None
     metadata: Optional[Dict[str, Any]] = None
 
-    # Reconstruction
     reconstructed: Optional[HSI] = None
 
-    # Metrics
     metrics: Dict[str, float] = field(default_factory=dict)
-
-    # Outputs
+    timestamp: Optional[str] = None
     output_dir: Optional[Path] = None
 
-    # Status
+    experiment_id: Optional[str] = None
+
     status: str = "initialized"
 
-    def setup_output_dir(self, base_dir: Path):
-        if self.experiment_id is None:
-            raise ValueError("experiment_id must be set")
-        
-        self.output_dir = base_dir / self.experiment_id
-        self.output_dir.mkdir(parents=True, exist_ok=True)
+    # helpers
 
     def update_status(self, status: str):
         if status not in VALID_STATUSES:
             raise ValueError(f"Invalid status: {status}")
         self.status = status
+
+    def update_id(self):
+        if self.timestamp is None:
+            raise ValueError(f"No timestamp generated")
+        
+        if self.tag is None:
+            self.experiment_id = f"exp_{self.timestamp}"
+        else:
+            self.experiment_id = f"exp_{self.timestamp}_{self.tag}"
+
 
 

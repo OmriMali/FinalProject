65d559d53086409814a8a607142ca25fc6f5690a
Almog Sade <almog2019@gmail.com>
2026-05-07 19:50:19 +0300

    Add HSI metadata object

--- src/core/hsi.py
+++ src/core/hsi.py
@@ -1,18 +1,16 @@
 import numpy as np
 from dataclasses import dataclass, field
+    
 
 @dataclass(frozen=True)
-class HSI:
+class HSIMetadata:
     """
-    Immutable hyperspectral image container.
-
-    Stores hyperspectral image data in (H, W, B) format together with
-    spectral wavelength information and acquisition metadata.
+    Metadata describing a hyperspectral image.
 
     Parameters
     ----------
-    data : np.ndarray
-        Hyperspectral image cube with shape (height, width, bands).
+    shape : tuple[int, int, int]
+        Shape of the original hyperspectral image, as (height, width, bands).
 
     wavelengths : np.ndarray
         Wavelength corresponding to each spectral band, in nanometers.
@@ -32,10 +30,10 @@ class HSI:
     section_idx : int | None, optional
         In case the hsi was divided to sections, section number.
 
-    metadata : dict, optional
+    attributes : dict, optional
         Additional dataset-specific metadata.
     """
-    data: np.ndarray
+    shape: tuple[int, int, int]
     wavelengths: np.ndarray
     bit_depth: int
     
@@ -44,17 +42,48 @@ class HSI:
     scene_name: str | None = None
     section_idx: int | None = None
 
-    metadata: dict = field(default_factory=dict)
+    attributes: dict = field(default_factory=dict)
 
     def __post_init__(self):
         """
         Validate object consistency after initialization.
         """
-        if self.data.ndim != 3:
-            raise ValueError("HSI data must have shape (H, W, B)")
+        if len(self.shape) != 3:
+            raise ValueError("Shape must be (H, W, B)")
         
-        if len(self.wavelengths) != self.data.shape[2]:
+        if len(self.wavelengths) != self.shape[2]:
             raise ValueError("Number of wavelengths must match spectral bands")
+        
+        if self.bit_depth <= 0:
+            raise ValueError("Bit depth must be positive")
+
+
+
+@dataclass(frozen=True)
+class HSI:
+    """
+    Immutable hyperspectral image container.
+
+    Stores hyperspectral image data in (H, W, B) format together with
+    metadata about the HSI.
+
+    Parameters
+    ----------
+    data : np.ndarray
+        Hyperspectral image cube with shape (height, width, bands).
+
+    metadata : HSIMetadata
+        Metadata about the HSI, like wavelengths, shape, scene_name...
+    """
+    data: np.ndarray
+    metadata: HSIMetadata
+
+    def __post_init__(self):
+        """
+        Validate object consistency after initialization.
+        """
+        if self.data.shape != self.metadata.shape:
+            raise ValueError("Metadata shape does not match data shape")
 
     @property
     def shape(self):

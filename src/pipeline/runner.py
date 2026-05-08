639ee5378e10894a4b9a5153ff24d202e9114e73
Almog Sade <almog2019@gmail.com>
2026-05-08 17:36:26 +0300

    Add first version of Runner

---
+++ src/pipeline/runner.py
@@ -0,0 +1,90 @@
+from datetime import datetime
+from time import perf_counter
+from dataclasses import replace
+from socket import gethostname
+
+from src.core.hsi import HSI
+from src.core.training_signals import TrainingSignals
+from src.core.results import CompressionRunResult, DictionaryTrainingResult, RunMetadata
+from src.metrics.base import Metric, MetricResult
+from src.compressors.base import Compressor
+from src.dictionary_trainers.base import DictionaryTrainer
+
+class Runner:
+
+    def run_compression(self,
+                        hsi: HSI, compressor: Compressor,
+                        metrics: list[Metric],
+                        tags: dict | None = None,
+                        ) -> CompressionRunResult:
+        """
+        Run a complete compression-decompression experiment.
+
+        Parameters
+        ----------
+        hsi : HSI
+            Hyperspectral image to compress.
+
+        compressor : Compressor
+            Compressor instance used for compression and decompression.
+
+        metrics : list[Metric]
+            Metrics to compute after reconstruction.
+
+        tags : dict | None, optional
+            Additional user-defined run tags.
+
+        Returns
+        -------
+        CompressionRunResult
+            Complete compression run result.
+        """
+        run_metadata = RunMetadata(
+            timestamp=datetime.now().isoformat(timespec="seconds"),
+            machine=gethostname(),
+            tags=tags or {}
+        )
+
+
+        start = perf_counter()
+        compressed = compressor.compress(hsi)
+        compression_time = perf_counter() - start
+
+        start = perf_counter()
+        reconstructed = compressor.decompress(compressed)
+        decompression_time = perf_counter() - start
+
+        partial = CompressionRunResult(
+            original=hsi,
+            compressed=compressed,
+            reconstructed=reconstructed,
+            run_metadata=run_metadata,
+        )
+
+        computed_metrics = {
+            metric.short_name: metric.compute(partial)
+            for metric in metrics
+        }
+
+        computed_metrics["COMP_TIME"] = MetricResult(name="compression time",
+                                                     short_name="COMP_TIME",
+                                                     value=float(compression_time),
+                                                     unit="s")
+        
+        computed_metrics["DECOMP_TIME"] = MetricResult(name="decompression time",
+                                                        short_name="DECOMP_TIME",
+                                                        value=float(decompression_time),
+                                                        unit="s")
+        
+        result = replace(partial, metrics=computed_metrics)
+
+        return result
+
+
+    
+    def run_dictionary_training(self,
+                                signals: TrainingSignals,
+                                trainer: DictionaryTrainer,
+                                metrics: list[Metric]
+                                ) -> DictionaryTrainingResult:
+        pass

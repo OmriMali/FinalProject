e6ea4154de7e167e7208a23163dd72e9d44ab928
Almog Sade <almog2019@gmail.com>
2026-05-16 20:07:29 +0300

    Update some __init__.py files

--- src/compressors/__init__.py
+++ src/compressors/__init__.py
@@ -1,3 +1,4 @@
 from src.compressors.hcs1d import HCS1D, HCS1DConfig
 from src.compressors.hcs3d import HCS3D, HCS3DConfig
 from src.compressors.ccsds123 import CCSDS123, CCSDS123Config
+from src.compressors import registry
--- src/dictionary_trainers/__init__.py
+++ src/dictionary_trainers/__init__.py
@@ -1,1 +1,2 @@
 from src.dictionary_trainers.k_svd import K_SVD, K_SVDConfig
+from src.dictionary_trainers import registry

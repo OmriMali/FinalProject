9389abcc62465750b9c9d6b1003b255876ee3cef
Almog Sade <almog2019@gmail.com>
2026-05-24 15:13:41 +0300

    Add script to split datasets to train and test

---
+++ scripts/train_test_split.py
@@ -0,0 +1,31 @@
+from src.preprocessing.split import create_section_split_csv
+
+
+def main():
+
+    # ===== Parameters =====
+    sections_dir = r"data\sections"
+
+    splits_dir = r"resources\splits"
+    split_name = "jasper_ridge_split_01"
+
+    scenes = ["JasperRidge",
+              #"MoffetField", 
+              # "Cuprite",
+              ]
+
+    test_ratio = 0.2
+    seed = 42
+
+    # ===== Split =====
+    create_section_split_csv(
+        output_path=fr"{splits_dir}\{split_name}.csv",
+        sections_dir=sections_dir,
+        datasets=scenes,
+        test_ratio=test_ratio,
+        seed=seed
+    )
+
+
+if __name__ == "__main__":
+    main()

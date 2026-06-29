bf4717aea34e4ccf6a13696be27a94b0209fb79c
Omri Mali <mali1@mail.tau.ac.il>
2026-06-29 15:53:36 +0300

    removed band bands from JasperRidge and trained new ksvd dictionary

--- scripts/share_dictionary.py
+++ scripts/share_dictionary.py
@@ -4,12 +4,12 @@ from dataclasses import replace
 def main():
 
     # ===== Source dictionary =====
-    source_dir = r"results\dictionary_training\ksvd\artifacts\ksvd\ksvd_book_parameter_selection_20260626_191210"
+    source_dir = r"results\dictionary_training\ksvd\artifacts\ksvd\ksvd_book_results_20260629_153935"
     source_name = "dictionary"
 
     # ===== Destination =====
     resources_dir = r"resources\dictionaries"
-    name = "jasper_ridge_split_01_K_5"
+    name = "jasper_ridge_split_01_project_book"
 
     # ===== Load dictionary =====
     dictionary = io.load_dictionary(source_dir, source_name)
